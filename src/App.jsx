19b063e0f795af2515c870a6957fb96276a54b50
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-01 00:13:04 +0530

    dark light toggle

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { useState } from 'react'
+import Header from './components/Header'
+import DetailPage from './pages/DetailPage'
+import SearchPage from './pages/SearchPage'
+
+function App() {
+
+  //const [data, setData] = useState([])
+  //const [limit, setLimit] = useState(12)
+  const [displayGrid, setdisplayGrid] = useState(true)
+  //const [detail, setDetail] = useState({})
+
+  function switchViewMode(id) {
+    if (id) {
+      setdisplayGrid(prevDisplayGrid => !prevDisplayGrid)
+      //setDetail(data.find(entry => entry.id === id) || null)
+    } else {
+      setdisplayGrid(true)
+    }
+  }
+
+  return (
+    <>
+      <Header switchViewMode={switchViewMode} />
+      {
+        displayGrid ? (
+          <SearchPage />
+        ) : (
+          <DetailPage />
+        )
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Search/SearchGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchGrid = () => {
+  return (
+    <div>SearchGrid</div>
+  )
+}
+
+export default SearchGrid
