4806cbc33e3eaeb3dd82e70a3d5c836d46fe0f90
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-01 16:19:24 +0530

    Update SearchGrid.jsx

--- src/components/Search/SearchGrid.jsx
+++ src/components/Search/SearchGrid.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Card from './Card';
 
-const SearchGrid = () => {
+const SearchGrid = (props) => {
+
+  function generateCards() {
+    const cardArray = [];
+    const filteredData = props.params !== null ? filterData() : props.data;
+
+    for (const entry of filteredData) {
+      cardArray.push (
+        <Card 
+            key={entry.id}
+            data={entry}
+            switchViewMode={props.switchViewMode}
+        />
+      )
+    }
+
+    return cardArray;
+
+  }
+
+  function filterData() {
+    const filteredData = [];
+    const fullTimeOnly = props?.params?.fullTimeOnly || null;
+    const job = props?.params?.job?.toLowerCase() || null;
+    const location = props?.params?.location?.toLowerCase() || null;
+
+    for (const entry of props.data) {
+      console.log();
+      if (fullTimeOnly && entry.contract !== "Full Time") continue;
+      if (location && entry.location.toLowerCase().includes(location) === false) continue;
+      if (
+        job &&
+        entry.company.toLowerCase().includes(job) === false &&
+        entry.position.toLowerCase().includes(job) === false &&
+        entry.requirements.content.toLowerCase().includes(job) === false &&
+        entry.requirements.items.join().toLowerCase().includes(job) === false &&
+        entry.role.content.toLowerCase().includes(job) === false &&
+        entry.role.items.join().toLowerCase().includes(job) === false
+      ) continue;
+
+      filteredData.push(entry);
+    }
+    return filteredData;
+  }
+
   return (
-    <div>SearchGrid</div>
+    <>
+    <div className="search-grid width-default">
+      {generateCards()}
+    </div>
+    </>
   )
 }
 
