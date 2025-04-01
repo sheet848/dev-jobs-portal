fd409e85316b7b857e2bb17610c84d1cdf7a2193
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-01 11:00:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ function App() {
 
   //const [data, setData] = useState([])
   //const [limit, setLimit] = useState(12)
-  const [displayGrid, setdisplayGrid] = useState(true)
+  const [displayGrid, setdisplayGrid] = useState(true);
   //const [detail, setDetail] = useState({})
 
   function switchViewMode(id) {
     if (id) {
-      setdisplayGrid(prevDisplayGrid => !prevDisplayGrid)
+      setdisplayGrid(prevDisplayGrid => !prevDisplayGrid);
       //setDetail(data.find(entry => entry.id === id) || null)
     } else {
-      setdisplayGrid(true)
+      setdisplayGrid(true);
     }
   }
 
