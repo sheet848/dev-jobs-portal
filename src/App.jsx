f8f92c6bdd0e62b534937979cdb9ce8a3098650a
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-01 16:17:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import SearchBar from '../components/Search/SearchBar';
-import SearchMobile from '../components/Search/SearchMobile';
-import SearchGrid from '../components/Search/SearchGrid';
+import './App.css'
+import { useEffect, useState } from 'react'
+import Header from './components/Header'
+import DetailPage from './pages/DetailPage'
+import SearchPage from './pages/SearchPage'
 
-const SearchPage = (props) => {
+function App() {
 
-    const [size, setSize] = useState(window.innerWidth);
-    const [params, setParams] = useState(null);
+  const [data, setData] = useState([]);
+  const [limit, setLimit] = useState(12);
+  const [displayGrid, setdisplayGrid] = useState(true);
+  //const [detail, setDetail] = useState({});
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [])
-
-    useEffect(() => {
-      function updateSize() {
-        setSize(window.innerWidth);
-      }
-      window.addEventListener("resize", updateSize);
-
-      return () => {
-        window.removeEventListener("resize", updateSize);
-      }
-    }, []);
-
-    function submitQuery(query) {
-      setParams({...query})
+  function switchViewMode(id) {
+    if (id) {
+      setdisplayGrid(prevDisplayGrid => !prevDisplayGrid);
+      //setDetail(data.find(entry => entry.id === id) || null);
+    } else {
+      setdisplayGrid(true);
     }
+  }
 
-    function handleClick(e) {
-      e.preventDefault();
-      e.target.blur();
-      props.requestMoreData();
-    }
+  useEffect(() => {
+    fetch('./constants/data.json', { mode: "no-cors"})
+      .then(res => res.json())
+      .then(jsonArray => {
+        const filterArray = jsonArray.slice(0, limit);
+        setData([...filterArray]);
+      })
+  }, [limit]);
+
+  function requestMoreData() {
+    setLimit(prevLimit => prevLimit + 6);
+  }
 
   return (
     <>
-    <main id="main">
+      <Header switchViewMode={switchViewMode} />
       {
-        size >=720 ? (
-          <SearchBar 
-            submitQuery={submitQuery}
-            placeholderText={ size >= 1200 ? "Filter by title, companies, expertise..." : "Filter by Title..."}
-            labelText={ size >= 1200 ? "Full Time Only" : "Full Time"} />
+        displayGrid ? (
+          <SearchPage
+            data={data}
+            requestMoreData={requestMoreData}
+            switchViewMode={switchViewMode}
+           />
         ) : (
-          <SearchMobile submitQuery={submitQuery} />
+          <DetailPage />
         )
       }
-        <SearchGrid 
-          data={props.data}
-          switchViewMode={props.switchViewMode}
-          params={params} 
-        />
-
-      {/*  <div className="load-more-btn-container">
-          <button className="btn btn-primary"
-              onClick={handleClick}>Load More</button>
-        </div>   
-      */}
-        <div className="attribution">
-          Challenge by {" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target='_blank'>
-            Frontend Mentor
-          </a>
-          . Coded by <a href='https://github.com/sheet848'>Sheetal Naik</a>
-        </div>
-    </main>
     </>
   )
 }
 
-export default SearchPage
+export default App
