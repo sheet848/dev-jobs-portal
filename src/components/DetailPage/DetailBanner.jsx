32d50aa48de9db5e3addc65205cd379a7c80d3db
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-02 15:41:29 +0530

    Update DetailBanner.jsx

--- src/components/DetailPage/DetailBanner.jsx
+++ src/components/DetailPage/DetailBanner.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
-const DetailBanner = () => {
+const DetailBanner = (props) => {
   return (
-    <div>DetailBanner</div>
+    <>
+      <section className="detail-banner width-small">
+        <div
+          className="banner-logo"
+          style={{ backgroundColor: props.data.logoBackground }}
+        >
+          <img
+            className="banner-logo-img"
+            src={`/${props.data.logo}`}
+            alt={`${props.data.company} company logo`}
+          />
+        </div>
+        <div className="banner-items-container">
+          <div className="banner-text-container">
+            <h2 className="banner-title">{props.data.company}</h2>
+            <p>{`${props.data.company.replace(" ", "").toLowerCase()}.com`}</p>
+          </div>
+          <a
+            href={props.data.website}
+            target="_blank"
+            className="btn btn-secondary"
+          >
+            Company Site
+          </a>
+        </div>
+      </section>
+    </>
   )
 }
 
